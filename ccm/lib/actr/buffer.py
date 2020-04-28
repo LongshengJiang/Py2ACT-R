003935c2000ac676dd91e527fec6be82d9d423a8
Student's <longshj@g.clemson.edu>
2020-04-27 22:42:26 -0400

    Add comments to increase readibility

--- ccm/lib/actr/buffer.py
+++ ccm/lib/actr/buffer.py
@@ -7,66 +7,124 @@ except:
 
 
 class Chunk(UserDict):
+  """Create a dictionary to store name-value pairs"""
   def __init__(self,contents,bound=None):
+    # We first initialize the UserDict.
     UserDict.__init__(self)
+    # If the contents is already in the form of a chunk, we update the current chunk with the name-value pairs in
+    # the contents.
     if isinstance(contents,Chunk):
       self.update(contents)
+    # If the contents is in the form of a string,
     elif isinstance(contents,str):
+      # We split the string by space. This create a list of string elements.
+      # For each element x and its index i,
       for i,x in enumerate(contents.split()):
+        # if there is ":" in element x, then x is a name-value pair. We split this name-value pair and denote the name
+        # as i and the value as the new x.
         if ':' in x:
           i,x=x.split(':',1)
+        # If x (or the new x) starts with "?", it means x is a variable in the from of "?xxx". In this case, we need
+        # to retrieve the value of the variable.
         if x.startswith('?'):
+          # Let the string after "?" to be the key for retrieving the variable value.
           key=x[1:]
+          # Retrieve the variable value from "bound". Note if there is a variable, the value of the variable must
+          # have been defined before. Hence, "bound" here will not be None.
           x=bound[key]
+        # We now save the retrieved name-value pair into the chunk. Note that index i may be int.
         self[i]=x
-    elif hasattr(contents,'__dict__'):  
+    # If the contents is a dictionary,
+    elif hasattr(contents,'__dict__'):
+      # We retrieve the key-value pairs using a for-loop.
       for k,v in contents.__dict__.items():
+        # If the type of the value is among the four following types,
         if type(v) in [str,float,int,bool]:
+          # Save the key-value pair into the chunk.
           self[k]=v
+    # For other types of contents,
     else:
+      # let us directly try to update the current chunk with the contents.
       try:
         self.update(contents)
+        # It may raise an exception, if the current chunk cannot be updated.
       except:  
         raise Exception('Unknown contents for chunk:',contents)      
   def __repr__(self):
+    # Create a list for storing the organized information of the chunck.
     r=[]
+    # Let us collect all the keys of the current chunk in a list.
     keys=self.keys()
+    # Define an int index.
     i=0
+    # As long as the index is an int, we add the value as a string to the list r.
     while i in keys:
-      r.append('%s'%self[i])      
+      r.append('%s'%self[i])
+      # Remove this int key from the list keys.
       keys.remove(i)
       i+=1
+    # After the above while-loop, there is no int in the list keys. We sort the keys in the list.
     keys.sort()
+    # For each key k in the list keys, if it does not start with '_', we append the key-value pair in the form of
+    # "name:value" to the list r.
     for k in keys:
       if k[0]!='_':
         r.append('%s:%s'%(k,self[k]))
+    # Finially, let us use a space as the seperator to join the elements in the list r into a string.
     return ' '.join(r)  
       
 
 class Buffer(ccm.Model):
   def __init__(self):
+    # Create the variable for storing a chunk.
     self.chunk=None
+
   def set(self,chunk):
+    """Add a chunk of information into the buffer"""
+    # Try to create the chunk of information. Here we use function Chunk() to reshape the information into a dictionary.
     try:
       self.chunk=Chunk(chunk,self.sch.bound)
     except AttributeError:
-      self.chunk=Chunk(chunk,{})  
+      # In the case that there is variables defined in the input argument "chunk", there is to self.sch.bound defined.
+      # Hence, we put {} for the second argument.
+      self.chunk=Chunk(chunk,{})
+
   def modify(self,**args):
+    """Modify the information in the buffer."""
+    # For the key-value pairs in the args,
     for k,v in args.items():
+      # if the key starts with an underscore "_", then the key uses an int as the index. We recover the int key from
+      # the original string key.
       if k.startswith('_'): k=int(k[1:])
+      # If the key is not in the dictionary self.chunk, we raise an exception saying there is no such slot.
       if k not in self.chunk:
         raise Exception('No slot "%s" to modify to "%s"'%(k,v))
+      # We then update the value index by the key k.
       self.chunk[k]=v
       self.chunk=self.chunk
+
   def __getitem__(self,key):
+    # This function return the value indexed by the key.
     return self.chunk[key]
+
   def clear(self):
-    self.chunk=None  
+    """Clear the buffer entirely."""
+    # Clear the chunk.
+    self.chunk=None
+
   def __eq__(self,other):
+    """Check if the info in the buffer is the same"""
+    # Return True if the chunk equals other; return False if the chunk does not equal other.
     return self.chunk==other
+
   def __hash__(self):
     return id(self)
+
   def __len__(self):
     if self.chunk is None: return 0
-    return len(self.chunk)  def isEmpty(self):
+    return len(self.chunk)
+
+  def isEmpty(self):
+    """Check if the buffer is empty."""
+    # Return True if the buffer is empty; return False if the buffer is not empty.
     return len(self)==0
--- ccm/lib/actr/vision.py
+++ ccm/lib/actr/vision.py
@@ -28,38 +28,63 @@ class Vision(ccm.Model):
     return self.now()<time+self.visualOnsetSpan
     
   def environmentUpdate(self):
-   while True:    
+    # First, create a while-loop for checking updates to the environment.
+   while True:
+     # If the visual location buffer is empty,
     if self._location.isEmpty():
-    
+      # Create a list for storing new visible objects in the environment.
       r=[]
+      # In the following self is the vision module, self.parent is the agent which includes the vision module,
+      # and self.parent.parent is the environment in which the agent is.
       for o in self.parent.parent.get_children():
+        # If the object is not visible anymore,
         if not getattr(o,'visible',True):
-            if o in self.timeAppeared.keys(): del self.timeAppeared[o] 
+            # further if the object is in timeAppeared, then we delete the object from timeAppeared.
+            # Note timeAppeared records the time when an object becomes visible in the environment.
+            if o in self.timeAppeared.keys(): del self.timeAppeared[o]
+            # Let us go to check the next object in the environment directly.
             continue
+        # If the object is not in timeAppeared,
         if o not in self.timeAppeared:
+          # further if the object has the location information,
           if hasattr(o,'x') and hasattr(o,'y'):  
-            #if self.lastLocationPattern.match(o)!=None:
+            #(obsolete) if self.lastLocationPattern.match(o)!=None:
+              # Let add this object into the list r.
               r.append(o)
+      # If there is new visible objects, i.e., the list is not empty,
       if len(r)>0:
-        obj=self.random.choice(r)    
+        # We randomly select one object.
+        obj=self.random.choice(r)
+        # We print the following information to the display.
         self.log._='Vision stuffed obj at (%g,%g)'%(obj.x,obj.y)
+        # We add the location of the new visible object to the visual location buffer.
         self._location.set('%g %g'%(obj.x,obj.y))
     
-    #print 'checking tracking',self.tracking  
-    #if self.tracking!=None and (self.tracking not in self.parent.parent.get_children() or not getattr(self.tracking,'visible',True)):
-    #  print '...lost'
-    #  self.sch.add(self.lostTrack,delay=0.085)
+    #(obsolete) print 'checking tracking',self.tracking
+    #(obsolete) if self.tracking!=None and (self.tracking not in self.parent.parent.get_children() or not getattr(self.tracking,'visible',True)):
+    #(obsolete)  print '...lost'
+    #(obsolete)  self.sch.add(self.lostTrack,delay=0.085)
+
+     # In the following self is the vision module, self.parent is the agent which includes the vision module,
+     # and self.parent.parent is the environment in which the agent is. In the for-loop, each object in the environment
+     # is checked to see if they are new visiable objects.
     for o in self.parent.parent.get_children():    
-      self.isNew(o)      
-      
+      self.isNew(o)
+    # In the following self is the vision module, self.parent is the agent which includes the vision module,
+    # and self.parent.parent is the environment in which the agent is.
     yield self.parent.parent.changes,self.lostTrack
    
   def lostTrack(self):
+    """Indicate that the vision tracking is lost."""
+    # If the vision module is tracking an object,
     if self.tracking is not None:
+      # The object slips away from our tracking.
       self.tracking=None
+      # We print the information to the display.
       self.log._='Object disappeared'
+      # And we clear the visual buffer.
       self._visual.clear()
-      #self._location.clear()
+      #(obsolete) self._location.clear()
 
     
   def attendToUnattended(self,pattern=''):
@@ -68,48 +93,95 @@ class Vision(ccm.Model):
     self.attendTo(pattern=pattern,new=True)
 
   def attendTo(self,pattern,unattended=False,new=False):
+    # The argument "pattern" contains the information of a visual location.
+    # If pat provides only the values of x-coordinate and y-coordinate,
+    # we reorganize pat into 'x: x-coordinate y: y-coordinate'.
     if isinstance(pattern, str) and ':' not in pattern and pattern.count(' ')==1:
         pattern='x:%s y:%s'%tuple(pattern.split(' '))
-  
+    # The x-coordinate, y-coordinate often are variables, e.g. "?x ?y". We use function Pattern() to map
+    # the variables to the values stored in sch.bound.
     self.lastLocationPattern=Pattern(pattern,self.sch.bound)
     r=[]
+    # In the following self is the vision module, self.parent is the agent which includes the vision module,
+    # and self.parent.parent is the environment in which the agent is. In the for-loop, each object in the environment
+    # is checked to see if they are visiable objects.
     for obj in self.parent.parent.get_children():
+      # If the visual location is new, but the object is not new, we directly go to check the next object.
       if new==True and not self.isNew(obj): continue
+      # If the location is unattended, but the object is in finst (finger instantiation) list, we directly go to check
+      # the next object.
       if unattended==True and self.finst.isIn(obj): continue
+      # Let us check if an object is a visible object. We do so by checking if the object contains the attributes
+      # "x", "y", and if the attribute "visible=True".
       if hasattr(obj,'x') and hasattr(obj,'y') and getattr(obj,'visible',True):
+        # Further, if the visual location matches the location of the visible object,
         if self.lastLocationPattern.match(obj)!=None:
+          # We add this object to the list r.
           r.append(obj)
+    # If we found more than one visible objects in the list r,
     if len(r)>0:
-      obj=self.random.choice(r)    
+      # We randomly chose one object from the list.
+      obj=self.random.choice(r)
+      # We print the following information to the display.
       self.log._='Vision found obj at (%g,%g)'%(obj.x,obj.y)
+      # We store the object's location into the visual location buffer. 
       self._location.set('%g %g'%(obj.x,obj.y))
 
   def isClose(self,a,b):
       return abs(a-b)<0.01
 
   def examine(self,pat):
+    """
+    Check the visible object at a visual location.
+    :param pat: specify the visual location.
+    :return:
+    """
+    # The argument "pat" (pattern) contains the information of a visual location.
+    # If pat provides only the values of x-coordinate and y-coordinate,
+    # we reorganize pat into 'x: x-coordinate y: y-coordinate'.
     if isinstance(pat, str) and ':' not in pat and pat.count(' ')==1:
         pat='x:%s y:%s'%tuple(pat.split(' '))
-    
+    # The x-coordinate, y-coordinate often are variables, e.g. "?x ?y". We use function Pattern() to map
+    # the variables to the values stored in sch.bound.
     self.lastExamine=Pattern(pat,self.sch.bound)
-    
+    # Now we update the variable pat which has assigned valules.
     pat=self.lastExamine
-
+    # In the following self is the vision module, self.parent is the agent which includes the vision module,
+    # and self.parent.parent is the environment in which the agent is. In the for-loop, each object in the environment
+    # is checked to see if they are visiable objects.
     for obj in self.parent.parent.get_children():
+     # First, let us check if an object is a visible object. We do so by checking if the object contains the attributes
+     # "x", "y", and if the attribute "visible=True".
      if hasattr(obj,'x') and hasattr(obj,'y') and getattr(obj,'visible',True):   
+      # Further, if the visual location pat matches the location of the visible object,
       if pat.match(obj) is not None:
-      #if self.isClose(obj.x,float(pat[0])) and self.isClose(obj.y,float(pat[1])):        
+      #(obsolete) if self.isClose(obj.x,float(pat[0])) and self.isClose(obj.y,float(pat[1])):
+        # The vision module starts working; it is busy.
         self.busy=True
+        # The default time required for a vision module request is set as 0.085s.
         yield 0.085
+        # The vision module finishes working; it is not busy.
         self.busy=False
+        # In the following, self is the vision module, self.parent is the agent which includes the vision module,
+        # and self.parent.parent is the environment in which the agent is.
+        # If obj is still an object in the environment,
         if obj in self.parent.parent.get_children():
+          # The vision module is tracking this object.
           self.tracking=obj
+          # We print the following information to the display.
           self.log._='Vision sees %s'%Chunk(obj)
+          # We put the object into the visual buffer.
           self._visual.set(obj)
+          # We add the object into the list of finst (finger instantiation).
           self.finst.add(obj)
+        # If obj is no longer an object in the environment,
         else:
-          self.tracking=None  
+          # There is nothing to track.
+          self.tracking=None
+          # We print the following information to the display.
           self.log._='Vision sees nothing'
+          # We clear the visual buffer.
           self._visual.clear()
+        # Each time, only one object can be seen. When we find one, we break the for-loop.
         break  
 
